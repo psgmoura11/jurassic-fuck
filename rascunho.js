d016c36564ac5af67014fefbb85d01b0190d1419
Paulo Moura <paulo.s.moura.11@gmail.com>
2021-08-14 23:40:19 +0100

    jurassic-fuck push to github

---
+++ rascunho.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//conecta a app ao backend(firebase)
+const firebaseConfig = {
+    apiKey: "AIzaSyCsBm3JU3RgyUIXdy9Q5LDR82ww9MkR96w",
+    authDomain: "tinder-clone-550a6.firebaseapp.com",
+    projectId: "tinder-clone-550a6",
+    storageBucket: "tinder-clone-550a6.appspot.com",
+    messagingSenderId: "531577429200",
+    appId: "1:531577429200:web:d51a53d1bf7176e47f52d5",
+    measurementId: "G-S2Z2B6CQXB"
+  };
